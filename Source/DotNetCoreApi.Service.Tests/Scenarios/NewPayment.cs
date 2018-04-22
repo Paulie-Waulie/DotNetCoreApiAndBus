9d6284fa2224f3b2196850ec903f3aed47850935
Paul Schoolden <paul.schoolden@sky.com>
2018-04-22 09:21:50 +0100

    Added service tests project

---
+++ Source/DotNetCoreApi.Service.Tests/Scenarios/NewPayment.cs
@@ -0,0 +1,13 @@
+﻿namespace DotNetCoreApi.Service.Tests.Scenarios
+{
+    using NUnit.Framework;
+
+    public class NewPayment
+    {
+        [Test]
+        public void Foo()
+        {
+            Assert.Fail("This test should fail.");
+        }
+    }
+}
